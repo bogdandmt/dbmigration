914d5d339bd4f3f9085ccbded692fc44fe2a3022
Bohdan <bogdandmt@gmail.com>
2017-05-18 15:17:27 +0300

    A base version

---
+++ src/main/resources/db/migration/V1__create_task.sql
@@ -0,0 +1,7 @@
+create table task (
+  id bigint(20),
+  description varchar(255),
+  name varchar(255),
+  creator varchar(255),
+  primary key (id)
+);
---
+++ src/main/resources/db/migration/V2__create_client.sql
@@ -0,0 +1,7 @@
+create table client (
+  id bigint(20),
+  firstname varchar(255),
+  lastname varchar(255),
+  phone varchar(255),
+  primary key (id)
+);
